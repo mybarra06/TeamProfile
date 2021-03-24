7cb152b8635be35733b3c5806c06b7e8332b1274
manny ybarra <manuelybarra2006@yahoo.com>
2021-03-24 00:34:52 -0700

    final

---
+++ _tests_/engineer.test.js
@@ -0,0 +1,22 @@
+const Engineer = require("../dist/lib/Engineer");
+
+describe("Engineer", () => {
+  test("Can set GitHub account with constuctor", () => {
+    const github = "GHUsername";
+    const e = new Engineer("Nick", 77, "something@email.com", github);
+
+    expect(e.github).toBe(github);
+  });
+  test("Can get role via getRole() method", () => {
+    const e = new Engineer("Nick", 77, "something@email.com", "github");
+
+    expect(e.getRole()).toBe("Engineer");
+  });
+  test("Can get GitHub via getGitHub() method", () => {
+    const github = "GHUsername";
+    const e = new Engineer("Nick", 77, "something@email.com", github);
+
+    expect(e.getGithub()).toBe(github);
+  });
+});
+
---
+++ index.js
@@ -0,0 +1,177 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const Engineer = require("./dist/lib/engineer");
+const Manager = require("./dist/lib/manager");
+const Intern = require("./dist/lib/intern");
+const Employee = require("./dist/lib/employee");
+// const makeHTML = require("index.html")
+
+const Employees = [];
+
+//Manager
+function getManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the employees name?",
+        name: "employeeName",
+      },
+      {
+        type: "input",
+        message: "What is your job title?",
+        name: "jobTitle",
+      },
+      {
+        type: "input",
+        message: "What is your ID?",
+        name: "employeeID",
+      },
+      {
+        type: "input",
+        message: "What is your e-mail?",
+        name: "employeeEmail",
+      },
+      {
+        type: "input",
+        message: "What is your office number?",
+        name: "officeNumber",
+      },
+    ])
+    .then((dataAnswers) => {
+      const managerData = new Manager(
+        dataAnswers.employeeName,
+        dataAnswers.employeeID,
+        dataAnswers.employeeEmail,
+        dataAnswers.officeNumber
+      );
+      Employees.push(managerData);
+
+      newMember();
+    });
+}
+
+function newMember() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What employee would you like to add?",
+        name: "newEmployee",
+        choices: ["Engineer", "Intern", "Done"],
+      },
+    ])
+    .then((eOrI) => {
+      switch (eOrI.newEmployee) {
+        case "Engineer":
+          getEngineer();
+          break;
+        case "Intern":
+          getIntern();
+          break;
+        default:
+          buildHTML();
+      }
+    });
+}
+
+function buildHTML() {
+console.log(Employees)
+}
+
+
+//Engineer
+function getEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the employees name?",
+        name: "employeeName",
+      },
+      {
+        type: "input",
+        message: "What is your job title?",
+        name: "jobTitle",
+      },
+      {
+        type: "input",
+        message: "What is your ID?",
+        name: "employeeID",
+      },
+      {
+        type: "input",
+        message: "What is your e-mail?",
+        name: "employeeEmail",
+      },
+      {
+        type: "input",
+        message: "What is your github username?",
+        name: "github",
+      },
+    ])
+    .then((engAnswers) => {
+      const engineerData = new Engineer(
+        engAnswers.employeeName,
+        engAnswers.employeeID,
+        engAnswers.employeeEmail,
+        engAnswers.github
+      );
+      Employees.push(engineerData);
+
+      newMember();
+    });
+}
+
+//Intern
+function getIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the employees name?",
+        name: "employeeName",
+      },
+      {
+        type: "input",
+        message: "What is your job title?",
+        name: "jobTitle",
+      },
+      {
+        type: "input",
+        message: "What is your ID?",
+        name: "employeeID",
+      },
+      {
+        type: "input",
+        message: "What is your e-mail?",
+        name: "employeeEmail",
+      },
+      {
+        type: "input",
+        message: "What school do you go to?",
+        name: "school",
+      },
+    ])
+    .then((intAnswers) => {
+      const internData = new Intern(
+        intAnswers.employeeName,
+        intAnswers.employeeID,
+        intAnswers.employeeEmail,
+        intAnswers.school
+      );
+      Employees.push(internData);
+
+      newMember();
+    });
+}
+getManager()
+
+// function buildTeam() {
+//   const pageContent = makeHTML(Employees);
+//   fs.writeFile("index.html", pageContent, (err) =>
+//   err ? console.log(err) : console.log("An index.html file was created.")
+//   );
+// }
+// buildTeam()
